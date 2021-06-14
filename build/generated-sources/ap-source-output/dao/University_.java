a7f1b86efeecd1d1fb74553261dd34c403b4d125
Stefanini <essantos@latam.stefanini.com>
2021-06-14 12:53:09 -0300

    codigo

---
+++ build/generated-sources/ap-source-output/dao/University_.java
@@ -0,0 +1,16 @@
+package dao;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-09T18:17:39")
+@StaticMetamodel(University.class)
+public class University_ { 
+
+    public static volatile SingularAttribute<University, String> address;
+    public static volatile SingularAttribute<University, String> name;
+    public static volatile SingularAttribute<University, String> description;
+    public static volatile SingularAttribute<University, Long> id;
+
+}
